99a34eaef4b2af6a30bf168734f7c3c000f3eaf0
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-14 19:31:58 -0400

    chore: prevent normalization of semver versioning

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ scripts = (
 
 setuptools.setup(
     name="google-cloud-testutils",
-    version=version,
+    version=setuptools.sic(version),
     long_description=readme,
     author="Google LLC",
     author_email="googleapis-packages@google.com",
